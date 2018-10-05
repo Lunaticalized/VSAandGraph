9001233fe9304fbf0fe702db7c2c88e036a84b20
Xiating Ouyang <xouyang@roman.cs.wisc.edu>
2018-10-04 20:14:15 -0500

    graph finished

---
+++ Graph.py
@@ -0,0 +1,78 @@
+class Graph:
+	def __init__(self):
+		self.n = 0
+		self.m = 0
+		self.edges = [[]]
+		self.adj = [[]]
+
+	def read(self, filename):
+		file = read(filename, 'r')
+		line = file.readline()
+		br = line.split()
+		self.n, self.m = int(br[0]), int(br[1])
+		edge = [[] for i in range(n)]
+		self.adj = [[False for i in range(n)] for j in range(n)]
+		for i in range(m):
+			line = file.readline()
+			br = line.split()
+			u, v = int(br[0]), int(br[1])
+			edge[u].append(v)
+			edge[v].append(u)
+			self.adj[u][v] = self.adj[v][u] = True
+		self.edges = edge
+		file.close()
+
+	def print_graph(self):
+		print self.n, self.m
+		for i in range(self.n):
+			print i, self.edges[i]
+
+	def erdos(self, n, p):
+		self.n = n
+		import random
+		edge = [[] for i in range(n)]
+		self.adj = [[False for i in range(n)] for j in range(n)]
+		cnt = 0
+
+		for i in range(n - 1):
+			for j in range(i + 1, n):
+				if random.random() < p:
+					edge[i].append(j)
+					edge[j].append(i)
+					self.adj[i][j] = self.adj[j][i] = True
+					cnt += 1
+		self.m = cnt
+		self.edges = edge
+	def isClique(self, v, c):
+		for u in c:
+			if not self.adj[u][v]:
+				return False
+		return True
+	def find_maximal_cliques(self, v):
+		vertices = [i for i in range(self.n)]
+		vertices.remove(v)
+		ret = []
+		clique = [v]
+		while len(vertices) > 0:
+			done = False
+			for u in self.edges[v]:
+				if self.isClique(u, clique) and u in vertices:
+					clique.append(u)
+					vertices.remove(u)
+					done = True
+					break
+			if not done:
+				ret.append(clique)
+				v = vertices.pop(0)
+				clique = [v]
+		if len(clique) > 0:
+			ret.append(clique)
+		return ret
+			
+
+
+if __name__ == "__main__":
+	g = Graph()
+	g.erdos(1000, 0.1)
+	g.print_graph()
+	print g.find_maximal_cliques(0)
